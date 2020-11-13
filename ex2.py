fbfeb18bc2acc1f893f369167112a1cadde27f86
Bar Yaacovi <baryaacovi7@gmail.com>
2020-11-13 12:06:12 +0200

    first commit

---
+++ ex2.py
@@ -0,0 +1,9 @@
+from numpy import *
+from skeleton import *
+
+P = [[None for j in range(2+1)] for i in range(5+1)]
+print(P)
+print(zeros((5+1, 2+1), dtype=int))
+
+asn2 = Assignment2()
+asn2.draw_sample_intervals(100, 3)
